2c65531deeec9876613760e9fc0d91748db8392b
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-08 15:11:33 +0900

    Initial Commit

---
+++ customModifier/customModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  customModifierApp.swift
+//  customModifier
+//
+//  Created by emi oiso on 2023/10/08.
+//
+
+import SwiftUI
+
+@main
+struct customModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
